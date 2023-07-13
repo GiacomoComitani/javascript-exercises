9997304b66e6b1016c48960b4acbecf69369a3b7
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2023-07-13 19:03:50 +0200

    add solution to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const string = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return string.split("").reverse().join("") === string;
 };
-
 // Do not edit below this line
 module.exports = palindromes;

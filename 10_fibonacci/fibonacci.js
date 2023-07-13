deeec55d10249ef78483c012928bff0f908de912
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2023-07-13 19:13:46 +0200

    add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+   if (num < 0 ) return 'OOPS';
+   if (num === 0) return 0;
+
+   let prev = 1;
+   let prePrev = 0;
+
+   for( let i = 1; i < num; i++){
+    let current = prev + prePrev;
+    prePrev = prev
+    prev = current
+   }
+
+   return prev;
+
 
 };
 

09145c43c623ee4d884fa885e2272581b1c4fe9a
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2023-07-13 16:34:59 +0200

    add solutionn of exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(element => {
+  sum += element;
+ });
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  let res = 1;
 
+  for (let i = 0; i < args.length; i++){
+    res *= args[i]
+  }
+  return res
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line

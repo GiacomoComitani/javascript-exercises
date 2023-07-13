e88e5a316ca5bc23cec39e4965ace8ea47f70ae7
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2023-07-13 22:08:09 +0200

    add solution of exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    let oldestPerson;
+    let maxAge = -Infinity;
+  
+    for (let i = 0; i < array.length; i++) {
+      const currentPerson = array[i];
+      const currentAge = calculateAge(currentPerson);
+  
+      if (!currentPerson.hasOwnProperty("yearOfDeath")) {
+        if (currentAge > maxAge) {
+          maxAge = currentAge;
+          oldestPerson = currentPerson;
+        }
+      } else {
+        const currentYearOfDeath = currentPerson.yearOfDeath;
+        const ageAtDeath = currentYearOfDeath - currentPerson.yearOfBirth;
+  
+        if (ageAtDeath > maxAge) {
+          maxAge = ageAtDeath;
+          oldestPerson = currentPerson;
+        }
+      }
+    }
+  
+    return oldestPerson;
+  };
+  
+  function calculateAge(person) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - person.yearOfBirth;
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
